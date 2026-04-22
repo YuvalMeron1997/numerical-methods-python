63e3f82841e5cccbcfdcb68d1a4f4fc12183a35b
Yuval Meron <153757907+YuvalMeron1997@users.noreply.github.com>
2026-04-23 01:40:53 +0300

    Update root_finding_methods.py

--- root_finding_methods.py
+++ root_finding_methods.py
@@ -1,50 +1,32 @@
 """
-In this assignment you should find the intersection points for two functions.
+Root-finding methods for identifying intersection points between two functions.
+
+This module implements numerical techniques such as bisection and
+Newton-Raphson to approximate intersection points within a given interval.
 """
+
 import numpy as np
-import time
-import random
 from collections.abc import Iterable
-import math
 
-
-class Assignment2:
-    def __init__(self):
-        """
-        Here goes any one time calculation that need to be made before
-        solving the assignment for specific functions.
-        """
-
+class RootFindingMethods:
     def intersections(self, f1: callable, f2: callable, a: float, b: float, maxerr=0.001) -> Iterable:
         """
-        Find as many intersection points as you can. The assignment will be
-        tested on functions that have at least two intersection points, one
-        with a positive x and one with a negative x.
-
-        This function may not work correctly if there is infinite number of
-        intersection points.
-
-
+        Finds approximate intersection points between two functions f1 and f2
+        within the interval [a, b].
+        
         Parameters
         ----------
-        f1 : callable
-            the first given function
-        f2 : callable
-            the second given function
-        a : float
-            beginning of the interpolation range.
-        b : float
-            end of the interpolation range.
+        f1, f2 : callable
+            Functions whose intersections are to be computed.
+        a, b : float
+            Interval bounds.
         maxerr : float
-            An upper bound on the difference between the
-            function values at the approximate intersection points.
-
-
+            Maximum allowed error.
+        
         Returns
         -------
-        X : iterable of approximate intersection Xs such that for each x in X:
-            |f1(x)-f2(x)|<=maxerr.
-
+        Iterable
+            Approximate intersection points.
         """
         intersections_list = []
         call_intersect_func = self.intersect_func(f1, f2)
@@ -111,12 +93,6 @@ class Assignment2:
         return right
 
     def newton_raphson_func(self, root, left, right, call_intersect_func, maxerr, list):
-        # if call_intersect_func(left) <= maxerr:
-        #     list.append(left)
-        #     return
-        # if call_intersect_func(right) <= maxerr:
-        #     list.append(right)
-        #     return
         r = root
         small_num = 0.000000000001
         derive = (call_intersect_func(r + small_num) / small_num) - (call_intersect_func(r) / small_num)
@@ -128,69 +104,3 @@ class Assignment2:
             return
         else:
             list.append(r)
-
-
-##########################################################################
-
-
-import unittest
-from sampleFunctions import *
-from tqdm import tqdm
-
-
-class TestAssignment2(unittest.TestCase):
-
-    def test_sqr(self):
-
-        ass2 = Assignment2()
-
-        f1 = np.poly1d([-1, 0, 1])
-        f2 = np.poly1d([1, 0, -1])
-
-        X = ass2.intersections(f1, f2, -1, 1, maxerr=0.001)
-        for x in X:
-            self.assertGreaterEqual(0.001, abs(f1(x) - f2(x)))
-
-    def test_poly(self):
-
-        ass2 = Assignment2()
-
-        f1, f2 = randomIntersectingPolynomials(10)
-
-        X = ass2.intersections(f1, f2, -1, 1, maxerr=0.001)
-
-        for x in X:
-            self.assertGreaterEqual(0.001, abs(f1(x) - f2(x)))
-
-    # def test1(self):
-    #     f1 = lambda x: 5
-    #     f2 = lambda x: x**2 - 3*x + 5
-    #     f3 = lambda x: math.sin(x**2)
-    #     f4 = lambda x: math.e**(-2*(x**2))
-    #     f5 = lambda x: np.arctan(x)
-    #     f6 = lambda x: (np.sin(x))/x
-    #     f7 = lambda x: 1 / (np.log(x))
-    #     f8 = lambda x: math.e**(math.e**(x))
-    #     f9 = lambda x: np.log(np.log(x))
-    #     # f10 = lambda x: math.sin(math.log(x))
-    #     f11 = lambda x: (2**(1/(x**2))) * (math.sin(1/x))
-    #     f12 = lambda x: x**7
-    #     f13 = lambda x: 2*(x**9)
-    #     f14 =lambda x: 5.5*x**10 - 0.1*x**9 + 1.7*x**8 - 8.15*x**7 - 0.65*x**6 + 0.4*x**5 + 1.2*x**4 + 0.75*x**3 - 2.90*x**2 + 0.34*x - 3.88
-    #     f15 = lambda x: math.sin(100*x)
-    #     f16 = lambda x: math.cos(100*x)
-    #
-    #     a = -20
-    #     b = 20
-    #
-    #     intersector = Assignment2()
-    #     intersector.intersections(f1, f5, a, b, maxerr=0.001)
-    #     # intersector.intersections(f2, f8, a, b, maxerr=0.001)
-    #     intersector.intersections(f4, f9, a, b, maxerr=0.001)
-    #     intersector.intersections(f6, f7, a, b, maxerr=0.001)
-    #     intersector.intersections(f5, f9, a, b, maxerr=0.001)
-    #     # intersector.intersections(f7, f11, a, b, maxerr=0.001)
-
-
-if __name__ == "__main__":
-    unittest.main()
